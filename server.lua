ec38f4dae29cef1c86382aad1cdb626eeca9e7c8
Aqua Devs <98275381+Aqua-Devs@users.noreply.github.com>
2022-09-12 11:28:27 +0200

    Update server.lua

--- server.lua
+++ server.lua
@@ -1,18 +1,8 @@
-ESX = nil
-TriggerEvent('hypex:getTwojStarySharedTwojaStaraObject', function(obj) ESX = obj end)
-
 RegisterServerEvent('aqua_antyping:checkPing')
 AddEventHandler('aqua_antyping:checkPing', function()
-    local xPlayer = ESX.GetPlayerFromId(source)
     local ping = GetPlayerPing(source)
     if ping > Config.Max then
-        xPlayer.triggerEvent('aqua_antyping:kickPlayer', ping)
+       DropPlayer(Config.Powiadomienie .. ping)
+       print("Kicked player with id " .. source .. " because of high ping - " .. ping .. "ms")
     end
-end)
-
-RegisterServerEvent('aqua_antyping:kickPlayerS')
-AddEventHandler('aqua_antyping:kickPlayerS', function(ping)
-    local xPlayer = ESX.GetPlayerFromId(source)
-    xPlayer.kick(Config.Powiadomienie .. ping)
-    print("Kicked player with id " .. source .. " because of high ping - " .. ping .. "ms")
 end)
