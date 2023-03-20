89154638c88a051d9c217ee86dabf9890dc5f8a4
someone <98275381+someone005@users.noreply.github.com>
2023-03-20 16:41:18 +0100

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,5 +1,5 @@
 Config = {}
 
-Config.Czas = 3000 -- every millisecond to check the player's ping. default is 3000 milliseconds = 3 seconds
+Config.Czas = 3000 -- every millisecond to check the player ping. default is 3000 milliseconds = 3 seconds
 Config.Max = 200 -- the highest ping a player can have
 Config.Powiadomienie = "Your ping is too high to play on the server -"
